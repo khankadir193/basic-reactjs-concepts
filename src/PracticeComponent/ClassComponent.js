3414f0041b269c4cf3cdc9ea0a0ec480f131101f
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-14 13:26:00 +0530

    increment and decrement with class components

--- src/PracticeComponent/ClassComponent.js
+++ src/PracticeComponent/ClassComponent.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react'
 
-class ClassComp extends Component
-{
-  constructor(){
+class ClassComp extends React.Component {
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      count: 0,
+    }
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
 
   }
 
-  render(){
-    return
-    (
-      <h1>This is the class components...</h1>
+  handleIncrement = (event) => {
+    console.log('event..??', event);
+    console.log('event.target.value', event.target.value);
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  handleDecrement = (event) => {
+    console.log('event..??', event);
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>count Numb:{this.state.count}</h1>
+
+        <button onClick={this.handleDecrement}>Decrement</button>
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
     )
   }
 }
